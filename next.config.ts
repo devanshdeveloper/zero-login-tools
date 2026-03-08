ba962c1b3291ac6c06702b4b2aa222762c68ba70
devanshdeveloper <65781793+devanshdeveloper@users.noreply.github.com>
2026-03-08 23:17:10 +0530

    fix: for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
